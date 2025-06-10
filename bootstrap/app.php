1556aa1f0616dd32d4c73d45e4776082f0c4c356
luckidubi <nwodochidubem1@gmail.com>
2025-06-09 17:49:43 -0700

    Remove unused imports in app.php for cleaner code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Configuration\Routing;
-use Illuminate\Foundation\Configuration\Application as AppConfig;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
